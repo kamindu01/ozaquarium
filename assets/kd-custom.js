c32c467264d7499725cd6954d1ee08f14330b0c5
kamindu <kamindu@itone.biz>
2023-04-23 01:50:01 +0530

    carousel script

---
+++ assets/kd-custom.js
@@ -0,0 +1,10 @@
+// brands carousel
+var swiper = new Swiper(".kd-brands-carousel", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
