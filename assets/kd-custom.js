85858e9d51d8017fbff10ee7e5687a304c38c3d1
kamindu <kamindu@itone.biz>
2023-04-23 02:06:49 +0530

    added autoplay

--- assets/kd-custom.js
+++ assets/kd-custom.js
@@ -1,10 +1,15 @@
 // brands carousel
 var swiper = new Swiper(".kd-brands-carousel", {
-    slidesPerView: 1,
+    slidesPerView: 3,
     spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+    },
     loop: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  });
+});
